c99271e37965d291745f0d13cdb82a60cabd47bd
Marchel Karuna <marchel.122140065@student.itera.ac.id>
2025-12-03 20:40:36 +0700

    Deploy ToDo App + Google Cloud

--- server/db.py
+++ server/db.py
@@ -1,9 +1,21 @@
 import mysql.connector
+import os
+import time
 
 def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="261003",
-        database="todo_db"
-    )
+    retries = 5
+    while retries > 0:
+        try:
+            connection = mysql.connector.connect(
+                host=os.environ.get("DB_HOST", "localhost"),
+                user=os.environ.get("DB_USER", "root"),
+                password=os.environ.get("DB_PASSWORD", "261003"),
+                database=os.environ.get("DB_NAME", "todo_db")
+            )
+            return connection
+        except mysql.connector.Error as err:
+            print(f"Gagal konek ke DB: {err}. Retrying in 5 seconds...")
+            retries -= 1
+            time.sleep(5)
+    
+    raise Exception("Tidak bisa terhubung ke Database setelah 5 kali percobaan.")
