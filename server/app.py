add6d27d472d5dcba4d9e0539c4f2ff1bcf456d3
Marchel Karuna <marchel.122140065@student.itera.ac.id>
2025-12-05 07:49:27 +0700

    Final

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from routes import routes
@@ -8,4 +10,5 @@ CORS(app)
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
--- server/db.py
+++ server/db.py
@@ -1,21 +1,16 @@
-import mysql.connector
 import os
-import time
+from google.cloud.sql.connector import Connector, IPTypes
+import pymysql
+
+connector = Connector()
 
 def get_connection():
-    retries = 5
-    while retries > 0:
-        try:
-            connection = mysql.connector.connect(
-                host=os.environ.get("DB_HOST", "localhost"),
-                user=os.environ.get("DB_USER", "root"),
-                password=os.environ.get("DB_PASSWORD", "261003"),
-                database=os.environ.get("DB_NAME", "todo_db")
-            )
-            return connection
-        except mysql.connector.Error as err:
-            print(f"Gagal konek ke DB: {err}. Retrying in 5 seconds...")
-            retries -= 1
-            time.sleep(5)
-    
-    raise Exception("Tidak bisa terhubung ke Database setelah 5 kali percobaan.")
+    conn = connector.connect(
+        os.environ["DB_INSTANCE"],  # instance connection name
+        "pymysql",
+        user=os.environ.get("DB_USER", "root"),
+        password=os.environ.get("DB_PASSWORD", ""),
+        db=os.environ.get("DB_NAME", "todo_db"),
+        ip_type=IPTypes.PRIVATE
+    )
+    return conn
